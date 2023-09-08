f57e0457180758880031d0251b06972895ed8230
Himanshi Rawat <himanshirawat604@gmail.com>
2023-09-08 12:35:24 +0530

    metrics started

--- src/Pages/Metrics.jsx
+++ src/Pages/Metrics.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 const Metrics = () => {
   return (
-    <div>Metrics</div>
+    <div>
+      <Navbar navColor={"text-black"} />
+      <div>
+        
+      </div>
+      <Footer />
+    </div>
   )
 }
 
