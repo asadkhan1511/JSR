6862e003f5568e5c57cd5dc290e671c86e50b88c
Shivang <125237608+Shivang-Rawat@users.noreply.github.com>
2023-09-21 12:16:19 +0530

    update ri page

--- src/Pages/EditRentals.jsx
+++ src/Pages/EditRentals.jsx
@@ -1,10 +1,83 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditRentals = () => {
     const {id} = useParams();
+    const [a,setA]=useState({
+        heading:"",
+        content: ""
+    });
+    const navigate=useNavigate();
+    const getData = async () => {
+        var { data } = await axios.get(`https://jsr-backend-x7rr.onrender.com/Rentals/${id}`);
+        setA(data);
+    }
+    useEffect(()=>{
+        getData();
+    },[]);
+    const updateRental = async() => {
+        // console.log(a);
+        const apiURL = `https://jsr-backend-x7rr.onrender.com/Rentals/${id}`;
+        axios
+          .put(apiURL, a)
+          .then(() => {
+          navigate('/rentals');
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
   return (
-    <div>EditRentals---{id}</div>
+    <>
+      <div className="flex justify-center items-center h-screen bg-indigo-400">
+        <div className="w-96 p-6 shadow-lg bg-white rounded-md">
+          <h1 className="text-3xl block text-center font-semibold">
+          Edit Rentals
+          </h1>
+          <hr className="mt-3" />
+          <div className="mt-3">
+            <label htmlFor="Heading" className="block text-base mb-2">
+              Heading
+            </label>
+            <input
+              type="text"
+              id="Heading"
+              value={a?.heading}
+              className="border w-full text-base px-2 py-1 focus:outline-none focus-ring-0 focus:border-gray-400"
+              placeholder="Enter Title"
+              onChange={(e) => setA({ ...a, heading: e.target.value })}
+            />
+          </div>
+
+          {true ? <div className="mt-3">
+            <label htmlFor="Content" className="block text-base mb-2">
+              Content
+            </label>
+            <input
+              type="text"
+              id="Content"
+              value={a?.content}
+              className="border w-full text-base px-2 py-1 focus:outline-none focus-ring-0 focus:border-gray-400"
+              placeholder="Enter #Tag1 htmlFor Superhero"
+              onChange={(e) => setA({ ...a, content: e.target.value })}
+            />
+          </div>: null}
+
+          <div className="mt-2">
+              <button
+                className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+                onClick={() => {
+                    updateRental();
+                }}
+              >
+                UPDATE
+              </button>
+            
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
