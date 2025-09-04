3e96f68afe3a53c52babd3d1cac2cac66018b44c
asadkhan1511 <asayarkhans@gmail.com>
2025-09-05 02:06:37 +0530

    fixed uppercase font

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -32,14 +32,20 @@ function Login() {
           <input
             type="text"
             placeholder="Username"
-            className="w-full mb-4 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            autoCapitalize="none"
+            autoCorrect="off"
+            style={{ textTransform: "none" }}
+            className="font2 w-full mb-4 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
 
           <div className="relative w-full mb-6">
             <input
               type={showPassword ? "text" : "password"}
               placeholder="Password"
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              autoCapitalize="none"
+              autoCorrect="off"
+              style={{ textTransform: "none" }}
+              className="font2 w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <span
               className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-gray-700"
